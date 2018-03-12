c11227084a3356c2fda158ac467483048e55597e
Vincent Weis <vrobweis@gmail.com>
2018-03-11 22:28:05 -0700

    Completed assignment 3 by setting up while loops and jQuery animations, along with automated class changes on the slides to determine animation direction

--- A3/script.js
+++ A3/script.js
@@ -40,8 +40,32 @@ var start = function()
 var goToSlide = function(n, d)
 {
   d = d || 0;
-  $("#ssContainer .slide").stop().fadeOut(d);
-  $("#ssContainer .slide:nth-of-type(" + n + ")").stop().fadeIn(d);
+ /* $("#ssContainer .slide").stop().show("slide, left, d")
+  $("#ssContainer .slide:nth-of-type(" + n + ")").stop().show("slide, left, d");
+*/
+$(".slide").stop().removeClass("currentSlide").removeClass("leftSlide").removeClass("rightSlide");
+
+$(".slide:nth-of-type(" + n + ")").addClass("currentSlide");
+$(".currentSlide").animate({
+  left: "0px"
+}, 500); 
+var l = 0
+while (l < n) {
+  $(".slide:nth-of-type(" + l + ")").addClass("leftSlide");
+  l++;
+}
+$(".leftSlide").animate({
+  left: "-100%"
+}, 500);
+var r = 0
+r = n + 1
+while (r < (allImages.length + 1)) {
+  $(".slide:nth-of-type(" + r + ")").addClass("rightSlide");
+  r++;
+}
+$(".rightSlide").animate({
+  left: "100%"
+}, 500); 
 
   $("#numContainer button").removeClass("active");
   $("#numContainer button:nth-of-type(" + n + ")").addClass("active");
